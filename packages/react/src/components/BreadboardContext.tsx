1b7e8925313c3c7eca065efcedb6ba089b5b4cbc
Joseph Mearman <joseph@mearman.co.uk>
2024-02-29 13:39:48 +0000

    Add BreadboardContext type export

--- packages/react/src/components/BreadboardContext.tsx
+++ packages/react/src/components/BreadboardContext.tsx
@@ -2,3 +2,4 @@ import React from "react";
 import { BreadboardContextType } from "../@types/types";
 
 export const BreadboardContext = React.createContext<BreadboardContextType | null>(null);
+export type BreadboardContext = typeof BreadboardContext;
